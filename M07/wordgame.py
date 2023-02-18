62ca5c130125c6359c266c4d76a4d0af7284155c
emilycordero <cordeemi031@gmail.com>
2023-02-18 00:23:36 -0800

    New New

--- M07/wordgame.py
+++ M07/wordgame.py
@@ -5,42 +5,30 @@ author: Emily Cordero
 Purpose: Create a word game.
 
  """
-# import random library
-import random
-
  # Welcome user to the word guessing game
 
 print('Welcome to the word guessing game! \n')
 
-yes_game = input('Are you ready to begin (yes/no)? ')
-# Declare variables
-programming_words = ['javascript', 'python', 'java', 'c++', 'c#', 'php', 'html', 'css']
-hint = ''
-count = 0
-correct_word = False
+play_again = input('Are you ready to begin (yes/no)? \n')
 
-# Print the hint
-while yes_game.lower() == 'yes':
-    # Create the random word
-    random_word = random.choices(programming_words)
-    print(random_word)
+# Declare variables
+secret = 'python'
+
+# While loop for game
+while play_again.lower() == 'yes':
+    count = 0
     
     # Ask for the guess
     guessed_word = input('What is your guess? ')
-    print(guessed_word)
 
-    while guessed_word != random_word:
+    while guessed_word.lower() != secret:
         count += 1
-        correct_word = False
+        print('This guess was not correct. ')
         guessed_word = input('What is your guess? ')
-        print(guessed_word)
 
-        if correct_word == True:
-            print(f'Congratulations! You guessed it! It took you {count} times. \n ')
-        else:
-            correct_word == False
+    print(f'Congratulations! You guessed it! It took you {count} times. \n ')
 
-    yes_game = input('Are you ready to play again (yes/no)? ')
+    play_again = input('Are you ready to play again (yes/no)? ')
 
 # Thank user
 print('Thank you for playing. ')
